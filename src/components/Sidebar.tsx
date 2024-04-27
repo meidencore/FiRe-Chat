2d620af17a3ef88c30f11b768c72254a206d0df5
Meidencore <danieleverdale@gmail.com>
2024-04-27 19:36:36 -0400

    sidebar styles done

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,12 @@
+import { Chats, Navbar, Search } from "./"
+
 const Sidebar = () => {
   return (
-    <div className='basis-1/3 bg-_dimDark'>Sidebar</div>
+    <div className='basis-1/3 bg-_dimDark'>
+      <Navbar />
+      <Search />
+      <Chats />
+    </div>
   )
 }
 
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,17 @@
 import Chat from './Chat'
+import Chats from './Chats'
+import Input from './Input'
+import Message from './Message'
+import Navbar from './Navbar'
+import Search from './Search'
 import Sidebar from './Sidebar'
 
 export {
     Chat,
+    Chats,
+    Input,
+    Message,
+    Navbar,
+    Search,
     Sidebar,
 }
