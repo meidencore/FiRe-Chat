9731d1c244ec78e5d137494749eb58f4c5471080
Meidencore <danieleverdale@gmail.com>
2024-04-27 14:57:45 -0400

    firebase base config and useAuth done

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,11 +1,11 @@
-import { auth } from "../firebaseConfig";
+import { auth } from "../services/utils/firebase.config";
 import { onAuthStateChanged } from "firebase/auth";
 import { useState, useEffect } from "react";
 
 export function useAuth () {
 
-    const [user, setUser] = useState(null)
-    const [authState, setAuthState ] = useState(undefined)
+    const [user, setUser] = useState<any | null>(null)
+    const [authState, setAuthState ] = useState<any | undefined>(undefined)
 
     useEffect(() => {
         return onAuthStateChanged(auth, (user) => {
