cec7ce850973e4233bf05d2b3f9ba92ed99f68cb
Meidencore <danieleverdale@gmail.com>
2024-04-28 12:46:10 -0400

    basic responsive done

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,12 +2,12 @@ import { profile } from "../assets"
 
 const Navbar = () => {
   return (
-    <div className="flex item-center justify-between text-_dimSoft bg-_dark h-12 p-3">
-      <span className="font-bold cursor-default">FiRe-Chat</span>
-      <div className="flex gap-3">
+    <div className="flex item-center justify-center md:justify-between  text-_dimSoft bg-_dark h-12 p-3">
+      <span className="hidden md:block md:font-bold md:cursor-default ">FiReChat</span>
+      <div className="flex gap-3 justify-evenly">
         <img src={profile} alt="profile" className="bg-_dimSoft h-6 w-6 rounded-full object-cover"/>
         <span className="font-normal text-base cursor-default">Meidencore</span>
-        <button className="bg-_dimDark text-_dimSoft text-xs border-none rounded cursor-pointer p-1">Logout</button>
+        <button className="bg-_dark text-_dimSoft text-xs border-none rounded cursor-pointer p-1 ss:static ss:bg-_dimDark absolute bottom-2 left-2">Logout</button>
       </div>
     </div>
   )
